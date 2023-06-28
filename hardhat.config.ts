0cfe83d5c951fdf04258e93298ae7c6b9f4099f2
JohnGuilding <jwguilding@gmail.com>
2023-06-28 20:49:00 +0100

    Move tests to non-conflicting dir for foundry init

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,37 +6,37 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 // tasks
-import "./src/tasks/accounts";
+import "./tests/tasks/accounts";
 
 const config: HardhatUserConfig = {
-  networks: {
-    hardhat: {
-      blockGasLimit: 30000000,
+    networks: {
+        hardhat: {
+            blockGasLimit: 30000000,
+        },
     },
-  },
-  solidity: {
-    version: "0.8.12",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 9999,
-      },
-      metadata: {
-        bytecodeHash: "none",
-      },
+    solidity: {
+        version: "0.8.12",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 9999,
+            },
+            metadata: {
+                bytecodeHash: "none",
+            },
+        },
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  typechain: {
-    outDir: "typechain-types",
-    target: "ethers-v5",
-  },
-  paths: {
-    tests: "./src/test",
-  },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: "USD",
+    },
+    typechain: {
+        outDir: "typechain-types",
+        target: "ethers-v5",
+    },
+    paths: {
+        tests: "./src/test",
+    },
 };
 
 export default config;
